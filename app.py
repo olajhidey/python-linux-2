f7a7aea4ae12a27dde419948fcb2ed98e1f62f31
olicodema <servicesoliviaa@gmail.com>
2021-04-06 12:43:18 +0000

    test works

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
-import bottle
+# import bottle
 from bottle import route, run, Response, template
 import json
 import image
+from flask import Flask
+app = Flask(__name__)
 
 def call_service():
     directoryName = 'photos'
     image.process(directoryName)
 
-@route('/')
+@app.route('/')
 def index():
     """Home page"""
     title = "Image Processor App"
     call_service()
     return template('index.tpl',data="Request completed!", title=title)
 
-if __name__ == '__main__':
-	run(host='0.0.0.0', port=8000, debug=True, reloader=True)
+# if __name__ == '__main__':
+# 	run(host='localhost', port=8000, debug=True, reloader=True)
 	
-serverApp = bottle.default_app()
+# serverApp = bottle.default_app()
