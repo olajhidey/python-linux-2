ed8d20d92cd8a848887c81d9f4e4ec8ebb906510
olicodema <servicesoliviaa@gmail.com>
2021-06-10 12:49:30 +0000

    deploy to portal

--- index.py
+++ index.py
@@ -1,22 +1,22 @@
-# import bottle
+import bottle
 from bottle import route, run, Response, template
 import json
 import image
-from flask import Flask
-app = Flask(__name__)
+# from flask import Flask
+# app = Flask(__name__)
 
 def call_service():
     directoryName = 'photos'
     image.process(directoryName)
 
-@app.route('/')
+@route('/')
 def index():
     """Home page"""
     title = "Image Processor App"
-    # call_service()
+    call_service()
     return template('index.tpl',data="Request completed!", title=title)
 
-# if __name__ == '__main__':
-# 	run(host='localhost', port=8000, debug=True, reloader=True)
+if __name__ == '__main__':
+	run(host='localhost', port=8000, debug=True, reloader=True)
 	
-# serverApp = bottle.default_app()
+serverApp = bottle.default_app()
