968ee0612ea1684781b154098087c1f8edadae1a
olicodema <servicesoliviaa@gmail.com>
2021-06-10 16:17:44 +0000

    worker please

--- app.py
+++ app.py
@@ -9,14 +9,14 @@ def call_service():
     directoryName = 'photos'
     image.process(directoryName)
 
-@route('/')
+@app.route('/')
 def index():
     """Home page"""
     title = "Image Processor App"
-    # call_service()
+    call_service()
     return template('index.tpl',data="Request completed!", title=title)
 
 # if __name__ == '__main__':
 # 	run(host='localhost', port=8000, debug=True, reloader=True)
 	
-serverApp = bottle.default_app()
+# serverApp = bottle.default_app()
